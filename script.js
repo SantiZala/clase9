d0c6ee1c6c9e0a643021bd2aaddbcc57099749d1
SantiZala <santiagozalazar12@gmail.com>
2023-05-23 22:23:24 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function(){
+  function showAlert() {
+    alert('¡Mirá como aprendí a poner un botón de alerta!');
+  }
+function showAlert() {
+      alert("¡Hola, esta es un alerta de JavaScrpt!")
+  }
+  
+const form = document.getElementById('myForm');
+function validateEmail(email) {
+  const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+  return regex.test(email)
+}
+function validateForm() {
+  const emailInput = document.getElementById('email');
+  const email = emailInput.value;
+  if (!validateEmail(email)) {
+    alert('Por favor ingrese un correo electrónico válido.');
+  } else {
+    alert('Correo electrónico enviado correctamente.');
+  } 
+}
+
+
+if (form) {
+  form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  validateForm();
+})
+
+}
+
+
+document.querySelector("button.button-menu-toggle")
+  .addEventListener("click", function() {
+         document.querySelector(".nav-links").
+                    classList.toggle("nav-links-responsive")
+  }
+)})
